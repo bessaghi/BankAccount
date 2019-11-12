c26f6f181f57267a8c07903e19d295fbd70527cf
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-12 14:08:25 +0100

    Use transaction in printStatement.

--- src/main/java/fr/lacombedulionvert/Account.java
+++ src/main/java/fr/lacombedulionvert/Account.java
@@ -25,18 +25,19 @@ public class Account {
     public void deposit(int amount) {
         balance += amount;
         LocalDate date = LocalDate.now();
-        statement += "\n" + date.toString() + " \t | " + amount + " \t | " + balance;
         transactions.add(new Transaction(Operation.DEPOSIT, date, amount, balance));
     }
 
     public void withdraw(int amount) {
         balance -= amount;
         LocalDate date = LocalDate.now();
-        statement += "\n" + date.toString() + " \t | -" + amount + " \t | " + balance;
         transactions.add(new Transaction(Operation.WITHDRAWAL, date, amount, balance));
     }
 
     public String printStatement() {
+        for (Transaction tr :  transactions){
+            statement += tr.toString();
+        }
         return statement;
     }
 }
