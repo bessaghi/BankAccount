46c5c0a5db39c807c4f8bfe452303e045a6fae6f
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-12 14:01:29 +0100

    Add fields into Transaction.

--- src/main/java/fr/lacombedulionvert/Transaction.java
+++ src/main/java/fr/lacombedulionvert/Transaction.java
@@ -3,7 +3,15 @@ package fr.lacombedulionvert;
 import java.time.LocalDate;
 
 public class Transaction {
-    public Transaction(Operation operation, LocalDate date, int amount, int balance) {
+    private final Operation operation;
+    private final LocalDate date;
+    private final int amount;
+    private final int balance;
 
+    public Transaction(Operation operation, LocalDate date, int amount, int balance) {
+        this.operation = operation;
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
     }
 }
