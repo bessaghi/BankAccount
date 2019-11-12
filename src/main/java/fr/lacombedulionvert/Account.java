01b489f1c9e3e11362ab44ca3a0b3f69cb3eceb2
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-12 11:06:53 +0100

    Refactor Account class

--- src/main/java/fr/lacombedulionvert/Account.java
+++ src/main/java/fr/lacombedulionvert/Account.java
@@ -2,13 +2,17 @@ package fr.lacombedulionvert;
 
 public class Account {
 
-    private int balance = 0;
+    private int balance;
+
+    public Account() {
+        balance = 0;
+    }
 
     public int getBalance() {
         return balance;
     }
 
-    public void deposit(int i) {
-        balance = i;
+    public void deposit(int amount) {
+        balance = amount;
     }
 }
