dd90e71edaf5990b0f2db9f567b38cd1f4318e38
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-12 11:03:00 +0100

    Refactoring test

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -6,8 +6,13 @@ public class AccountTest {
 
     @Test
     public void new_account_has_empty_balance() {
+        // Given
         Account account = new Account();
 
-        Assertions.assertThat(account.getBalance()).isEqualTo(0);
+        // When
+        int actualBalance = account.getBalance();
+
+        // Then
+        Assertions.assertThat(actualBalance).isEqualTo(0);
     }
 }
