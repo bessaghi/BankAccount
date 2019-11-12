55153a01bf3816afb143b7194349f9a1140ca32e
bouchra94 <essaghir.bouchra@gmail.com>
2019-11-12 11:01:53 +0100

    Renaming test

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class AccountTest {
 
     @Test
-    public void test() {
+    public void new_account_has_empty_balance() {
         Account account = new Account();
 
         Assertions.assertThat(account.getBalance()).isEqualTo(0);
