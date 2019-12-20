a7cd85a6a25e92323829bc1a1594c1389907888a
bouchra94 <essaghir.bouchra@gmail.com>
2019-12-20 16:12:20 +0100

    Add class Formatter

--- src/main/java/fr/lacombedulionvert/Transactions.java
+++ src/main/java/fr/lacombedulionvert/Transactions.java
@@ -1,23 +1,18 @@
 package fr.lacombedulionvert;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static java.lang.String.join;
-import static java.util.Collections.reverse;
 
 class Transactions {
-
-    private static final String DELIMITER = "\n";
 
     private Map<Transaction, Integer> transactions;
     private int latestBalance;
 
     Transactions() {
-        transactions = new LinkedHashMap<>();
         latestBalance = 0;
+        transactions = new LinkedHashMap<>();
     }
 
     void add(Transaction transaction) {
@@ -25,21 +20,8 @@ class Transactions {
         transactions.put(transaction, latestBalance);
     }
 
-    private List<String> reverseTransactions() {
-        List<String> transactionsList = transformMapToList();
-        reverse(transactionsList);
-        return transactionsList;
-    }
-
-    private List<String> transformMapToList() {
-        return transactions.entrySet()
-                .stream()
-                .map(e -> e.getKey().toString() + e.getValue())
-                .collect(Collectors.toList());
-    }
-
     @Override
     public String toString() {
-        return join(DELIMITER, reverseTransactions());
+        return new Formatter(transactions).print();
     }
 }
