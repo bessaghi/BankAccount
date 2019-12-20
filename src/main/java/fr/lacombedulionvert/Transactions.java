79d2cbed3bacd62ebc34cde554e9b33952182976
bouchra94 <essaghir.bouchra@gmail.com>
2019-12-20 15:55:29 +0100

    Extract methods

--- src/main/java/fr/lacombedulionvert/Transactions.java
+++ src/main/java/fr/lacombedulionvert/Transactions.java
@@ -1,12 +1,12 @@
 package fr.lacombedulionvert;
 
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.joining;
+import static java.lang.String.join;
+import static java.util.Collections.reverse;
 
 class Transactions {
 
@@ -25,10 +25,21 @@ class Transactions {
         transactions.put(transaction, latestBalance);
     }
 
+    private List<String> reverseTransactions() {
+        List<String> transactionsList = transformMapToList();
+        reverse(transactionsList);
+        return transactionsList;
+    }
+
+    private List<String> transformMapToList() {
+        return transactions.entrySet()
+                .stream()
+                .map(e -> e.getKey().toString() + e.getValue())
+                .collect(Collectors.toList());
+    }
+
     @Override
     public String toString() {
-        List<String> reversed = transactions.entrySet().stream().map(e -> e.getKey().toString() + e.getValue()).collect(Collectors.toList());
-        Collections.reverse(reversed);
-        return String.join(DELIMITER, reversed);
+        return join(DELIMITER, reverseTransactions());
     }
 }
